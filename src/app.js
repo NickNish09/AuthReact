0420541cc32d84f2903154c37d465326e24fc615
NickNish09 <nicholasmarques@cjr.org.br>
2017-07-20 19:02:19 -0300

    feito formulario de login com aparencia daora

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+
 class App extends Component {
+	componentWillMount(){ //metodo invocado automaticamente antes do render
+		firebase.initializeApp({
+		    apiKey: 'AIzaSyBjxcm2hNhrq0r2u2o08Yb3KuUAfSTebNk',
+		    authDomain: 'authentication-5ac4f.firebaseapp.com',
+		    databaseURL: 'https://authentication-5ac4f.firebaseio.com',
+		    projectId: 'authentication-5ac4f',
+		    storageBucket: 'authentication-5ac4f.appspot.com',
+		    messagingSenderId: '871435131630'
+  		});
+	}
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>Oi</Text>
+				<LoginForm />
 			</View>
 		);
 	}
